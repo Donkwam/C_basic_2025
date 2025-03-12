95e1fda469030b3399f25bb8b5ee3df344b21cf5
Woo Dong Kwan <ehdrhks0509@naver.com>
2025-03-12 09:50:11 +0900

    2일차

--- scanf2.c
+++ scanf2.c
@@ -1,17 +1,46 @@
-#include <stdio.h>
+//#include <stdio.h>
+//
+//int main()
+//{
+	//// 1. 이름을 입력받아서 출력
+	//char str[20];
+	//printf("문자열을 입력하세요: ");
+	//scanf_s("%s", str, sizeof(str));	// sizeof는 배열의 크기를 반환하는 연산자
 
-int main()
-{
-	char str[20];
-	printf("문자열을 입력하세요: ");
-	scanf_s("%s", str, sizeof(str));	// sizeof는 배열의 크기를 반환하는 연산자
+	//printf("입력한 문자열: %s\n", str);
 
-	printf("입력한 문자열: %s\n", str);
-
+	//// 각각의 자료형의 크기 
 	/*printf("char 크기: %d\n", sizeof(char));
 	printf("정수 크기: %d\n", sizeof(int));
 	printf("float 크기: %d\n", sizeof(float));
 	printf("double 크기: %d\n", sizeof(double));*/		// 주석 지울때 Ctrl+K+C, 주석복구는 Ctrl+K+U
 
-	return 0;	
-}
+	//// 2. 이름과 나이를 입력받아서 출력
+	//int age;
+	//char str2[20];
+	//printf("이름과 나이를 입력하세요: ");
+	//scanf_s("%s, %d", str2, sizeof(str2), &age);
+
+	//printf("이름은 %s이고 나이는 %d입니다.\n", str2, age);
+
+
+	//// 3. 나이와 이름을 입력받아서 출력
+	//int age;
+	//char str3[20];
+	//printf("나이와 이름을 입력하세요: ");
+	//scanf_s("%d %s", &age, str3, sizeof(str3));
+
+	//printf("나이는 %d이고 이름은 %s입니다.\n", age, str3);
+
+
+//	// 4. 각각 입력받아서 출력
+//	char str4[20];
+//	int age;
+//	printf("나이: ");
+//	scanf_s("%d", &age);
+//	printf("이름: ");
+//	scanf_s("%s", str4, sizeof(str4));	// 벼열자체가 주소이기 때문에 &를 붙이지 않는다.
+//	printf("이름: %s, 나이: %d\n", str4, age);
+//
+//	return 0;	
+//}
