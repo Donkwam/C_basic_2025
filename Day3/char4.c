916ef836f0f89b34a877fee957d764b4d7ca73da
Woo Dong Kwan <ehdrhks0509@naver.com>
2025-03-13 17:27:12 +0900

    3일차

---
+++ Day3/char4.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	char ch2;
+	
+	printf("대소문자 하나를 입력하세요. 종료하고 싶을때 0 입력");
+	while (1) {
+		scanf(" %c", &ch2);		// 문자열이 하나이므로 %c를 사용한다.
+		if (('A' <= ch2) && (ch2 <= 'Z')) {
+			printf("대문자: %c\n", ch2);
+		}
+		else if (('z' >= ch2) && (ch2 > 'Z')) {
+			printf("소문자: %c\n", ch2);
+		}
+		else if (ch2 == '0') {
+			printf("종료");
+				break;
+		}
+		else {
+			printf("잘못입력하셨습니다.");
+		}
+	}
+	return 0;
+}
