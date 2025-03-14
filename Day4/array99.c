9ecee2d118267ad0856036eca9a76c6ec6505a91
Woo Dong Kwan <ehdrhks0509@naver.com>
2025-03-14 17:29:45 +0900

    4

--- Day4/array99.c
+++ Day4/array99.c
@@ -1,21 +1,21 @@
-﻿//#include <stdio.h>
-//
-//int main()
-//{
-//// 4사람의 3과목 점수를 받아서 학생별로 총점
-//int ary6[5][4] = { {0,},{0,},{0,},{0,} };
-//printf("학생들의 점수를 입력하세요.");
-//while (1) {
-//	for (int i = 0; (i + 1) < 5; i++) {
-//		int k = 0;
-//		for (int j = 0; (j + 1) < 4; j++) {
-//			int m = 0;
-//			scanf_s("%d", m);
-//			ary6[i][j] = m;
-//			k += ary6[i][j];
-//		}
-//		printf("%d, ", k);
-//	}
-//}
-//return 0;
-//}
+﻿#include <stdio.h>
+
+int main()
+{
+// 4사람의 3과목 점수를 받아서 학생별로 총점
+int* ary6[5][4] = { {0,},{0,},{0,},{0,} };
+printf("학생들의 점수를 입력하세요.");
+while (1) {
+	for (int i = 0; (i + 1) < 5; i++) {
+		int k = 0;
+		for (int j = 0; (j + 1) < 4; j++) {
+			int m = 0;
+			scanf_s("%d", m);
+			*ary6[i][j] = m;
+			k += *ary6[i][j];
+		}
+		printf("%d, ", k);
+	}
+}
+return 0;
+}
