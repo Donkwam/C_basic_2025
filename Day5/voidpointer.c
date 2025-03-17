d629107c18a0244ce5367fbce7123d33ef5aa9f2
Woo Dong Kwan <ehdrhks0509@naver.com>
2025-03-17 11:09:18 +0900

    5일차

---
+++ Day5/voidpointer.c
@@ -0,0 +1,31 @@
+﻿///*
+//	함수 포인터
+//*/
+//#include <stdio.h>
+//
+//int sum(int, int);
+//int min(int, int);
+//
+//int main()
+//{
+//	int (*fp)(int, int); // 함수 포인터를 이용하여서 호출받는다.
+//	int res;
+//	//res = sum(20, 10);
+//	//res = min(20, 10);
+//	fp = sum;
+//	res = fp(20, 10); 
+//	printf("res: %d\n", res);
+//	return 0;
+//}
+//
+//int sum(int a, int b)
+//{
+//	int res = a + b;
+//	return res;
+//}
+//
+//int min(int a, int b)
+//{
+//	int res = a - b;
+//	return res;
+//}
---
+++ Day5/voidpointer99.c
@@ -0,0 +1,34 @@
+﻿//#include <stdio.h>
+//void func(int(*fp)(int, int));
+//
+//int main()
+//{
+//	char a;
+//	int c, d;
+//	printf("연산할 두 값을 입력하세요.");
+//	scanf("숫자 두개 입력하세요: %d %d", &c, &d);
+//    scanf("연산 방법을 적으세요: %s", &a);
+//	
+//    int result = func(int(*fp)(int, int));
+//    printf("결과: %d\n", result);
+//
+//	return 0;
+//}
+//
+//void func(int(*fp)(int, int))
+//{
+//    switch (fp) {
+//        case '+': return a + b;
+//        case '-': return a - b;
+//        case '*': return a * b;
+//        case '/':
+//            if (b != 0) return a / b;
+//            else {
+//                printf("오류: 0으로 나눌 수 없습니다.\n");
+//                return 0;
+//            }
+//        default:
+//            printf("오류: 지원하지 않는 연산자입니다.\n");
+//            return 0;
+//    }
+//}
