7589189bf7d70b82e6ea1a2afa4596f6a10a48e4
Woo Dong Kwan <ehdrhks0509@naver.com>
2025-03-18 11:11:16 +0900

    1일차

---
+++ C++ Day1/cin.cpp
@@ -0,0 +1,25 @@
+﻿///*
+//	cin - 입력을 담당하는 객체
+//*/
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	//int inVal;
+//	//cout << "숫자 입력>> ";
+//	//cin >> inVal;		// 입력을 받는 객체
+//	//cout << "inVal: " << inVal << endl;
+//	int num, num2, sum;
+//	cout << "두개의 정수를 입력하세요>> ";
+//	cin >> num >> num2;
+//	cout << "합은: " << num + num2 << endl;
+//
+//	return 0;
+//}
+
+/* 
+	함수의 다형성
+	- 함수 오버로딩(함수의 다중정의)
+	- 함수 오버라이딩(함수의 재정의)
+*/
