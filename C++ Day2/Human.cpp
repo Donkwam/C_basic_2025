25075398ed791f6e1de9d1ac6fe50cbf61b9686f
Woo Dong Kwan <ehdrhks0509@naver.com>
2025-03-19 18:13:44 +0900

    Update Human.cpp

--- C++ Day2/Human.cpp
+++ C++ Day2/Human.cpp
@@ -11,9 +11,16 @@ private:
 	int age;
 	char* ptel;
 public:
-	Human(const char* p, int a, const char* pt) : age(a) { pname = new char[strlen(p) + 1]; strcpy(pname, p); 
-	ptel = new char[strlen(pt) + 1]; strcpy(ptel, pt); }
-	~Human() { delete[]pname; delete[]ptel; }
+	Human(const char* p, int a, const char* pt) : age(a) { 
+		pname = new char[strlen(p) + 1]; 
+		strcpy(pname, p); 
+		ptel = new char[strlen(pt) + 1]; 
+		strcpy(ptel, pt); 
+	}
+	~Human() { 
+		delete[]pname; 
+		delete[]ptel; 
+	}
 	
 	void getData(){
 		cout << "이름: " << pname << "  나이: " << age << "  전화번호: " << ptel << endl;
