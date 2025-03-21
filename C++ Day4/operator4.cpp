e7e805ef4f2c70e1ee897d441da5c1c6dfd478a9
Woo Dong Kwan <ehdrhks0509@naver.com>
2025-03-21 17:03:26 +0900

    4일

---
+++ C++ Day4/operator4.cpp
@@ -0,0 +1,42 @@
+﻿///*
+//*/
+//#include <iostream>
+//
+//class MyClass {
+//private:
+//	int x, y;
+//public:
+//	MyClass(int ax = 0, int ay = 0) : x(ax), y(ay) {}
+//
+//	MyClass (const MyClass& other) {
+//		x = other.x;
+//		y = other.y;
+//	}
+//	MyClass operator+(const MyClass& other) {
+//		printf("+ overloading\n");
+//		return MyClass(x + other.x, y + other.y);
+//	}
+//	MyClass add(const MyClass& other) {
+//		return MyClass(x + other.x, y + other.y);
+//	}
+//
+//	void showMyClass() {
+//		printf("x: %d, y: %d", x, y);
+//	}
+//};
+//
+//int main()
+//{
+//	MyClass obj(10, 20);
+//	MyClass obj2(20, 30);
+//	MyClass obj3 = obj.add(obj2);
+//	obj3.showMyClass();
+//	MyClass obj4 = obj.operator+(obj3);
+//	obj4.showMyClass();
+//	MyClass obj5 = obj + 100;
+//	obj5.showMyClass();
+//	return 0;
+//}
+//
+//// 오버로딩할 수 없는 연산자:
+//// ::, ., *, sizeof
---
+++ C++ Day4/template5.cpp
@@ -0,0 +1,24 @@
+﻿/*
+	템플릿 매개변수
+*/
+#include <iostream>
+
+template <typename T, int sz>
+class CTest {
+ptivate:
+	T ary[sz];
+public:
+	T& operator[](int idx) {
+		if (idx < 0 || idx >= sz) {
+			cout << "Error" << endl;
+			exit(1):
+		}
+		return ary[idx];
+	}
+};
+
+int main()
+{
+
+	return 0;
+}
