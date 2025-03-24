06130fc1aa71956cd8fc994d9779ebbf5a772dd9
Woo Dong Kwan <ehdrhks0509@naver.com>
2025-03-24 17:30:40 +0900

    55

---
+++ C++ Day5/objpointer6.cpp
@@ -0,0 +1,25 @@
+﻿/*
+	다운캐스팅 : 자식 포인터로 부모 객체를 가리킬 수 없다.
+*/
+#include <iostream>
+
+class Base {
+	void show() { printf("Base Class"); }
+};
+class Derived : public Base {
+public:
+	void show() { printf("Dervied class"); }
+};
+
+int main()
+{
+	Base bobj;
+	Derived* dptr = nullptr;
+	//dptr = &bobj
+	/*dptr = (Base*)&bobj;
+	dptr->show();*/
+
+	dptr = (Derived*)&bobj;
+	dptr->show();
+	return 0;
+}
